15bedbca76cabc958317b763b7a7147740e902fe
JorgeF010 <figueroajorge010@gmail.com>
2021-02-04 15:43:55 -0500

    First Commit
    Making a website that displays cryptocurrencies & news

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export const Navbar = () => {
     return (
       <Nav>
         <Logo href="/">
-            ECashNews
+            CryptoNews
         </Logo>
         <Hamburger onClick={() => setIsOpen(!isOpen)}>
           <span />
